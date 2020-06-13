a9eb8b206d1b417e630fb485648cdb7d1b34a1ac
devdigital <devdigital.cloud@gmail.com>
2020-06-13 20:33:29 +0100

    feat: add logger

---
+++ packages/logging/src/default-log.js
@@ -0,0 +1,24 @@
+const errorToParams = (error) => ({
+  errorName: error.name,
+  errorMessage: error.message,
+  stackTrace: error.stack,
+});
+
+export const defaultLog = ({ level, message, params, error, context }) => {
+  const parameters = error
+    ? {
+        ...(params || {}),
+        ...errorToParams(error),
+      }
+    : params || {};
+
+  const data = {
+    level,
+    timestamp: new Date().toISOString(),
+    message,
+    ...parameters,
+    ...(context || {}),
+  };
+
+  console.log(JSON.stringify(data));
+};
